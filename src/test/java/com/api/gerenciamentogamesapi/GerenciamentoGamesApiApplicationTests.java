bf81c1bc4ed6aeae6c9260bf60f7c42b1f4a2c6a
Tiago <tiago.polemarxgroup@gmail.com>
2024-11-15 13:24:18 -0300

    Project created

---
+++ src/test/java/com/api/gerenciamentogamesapi/GerenciamentoGamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gerenciamentogamesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoGamesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
